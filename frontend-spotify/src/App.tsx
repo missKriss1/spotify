5785b1c344255bc21afe942cddf3712262bd390f
Kris <kristinaovchinnikova22@gmail.com>
2025-01-15 01:02:48 +0600

    app init from frontend

--- frontend-spotify/src/App.tsx
+++ frontend-spotify/src/App.tsx
@@ -1,7 +1,16 @@
+import { CssBaseline } from '@mui/material';
+import AppToolBar from './components/UI/AppToolBar/AppToolBar.tsx';
 
 const App = () => {
   return(
     <>
+      <CssBaseline/>
+      <header>
+        <AppToolBar/>
+      </header>
+      <main>
+
+      </main>
     </>
   )
 }
---
+++ frontend-spotify/src/components/UI/AppToolBar/AppToolBar.tsx
@@ -0,0 +1,28 @@
+import { AppBar, Container, styled, Toolbar, Typography } from '@mui/material';
+import { Link as NavLink } from "react-router-dom";
+
+const Link = styled(NavLink)({
+  color: "inherit",
+  textDecoration: "none",
+  "&:hover": {
+    color: "inherit",
+  },
+});
+
+const AppToolBar = () => {
+  return (
+    <div>
+      <AppBar position="sticky" sx={{ mb: 2 }}>
+        <Container>
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Link to="/">Spotify</Link>
+            </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
+};
+
+export default AppToolBar;
